369856fb1d10995b492c2015c605a2ef22f3461a
Onur Gunes <onurgunes1993@gmail.com>
2022-01-29 16:25:23 +0100

    formatted

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import spacy
 
+
 def ner(string, lang):
 
     model = f'{lang}_core_web_sm'
@@ -11,19 +12,20 @@ def ner(string, lang):
     for token in parsed:
 
         text = token.text
-        #Not sure what you mean by type, so including a few possiblities
-        word_type =  token.ent_type_, token.dep_, token.head.pos_
+        # Not sure what you mean by type, so including a few possiblities
+        word_type = token.ent_type_, token.dep_, token.head.pos_
         start_pos = token.idx
         end_pos = start_pos + len(text)
 
-        out_dict = {'text':text,
-                    'type':word_type,
-                    'start_pos':start_pos,
-                    'end_pos':end_pos}
-        
+        out_dict = {'text': text,
+                    'type': word_type,
+                    'start_pos': start_pos,
+                    'end_pos': end_pos}
+
         out_list.append(out_dict)
 
     print(out_list)
     return(out_list)
 
-ner('I have $5 today','en')
+
+ner('I have $5 today', 'en')
