f70f53eb93b722d791599ccf4ae4148e63c3c0dd
Onur Gunes <onurgunes1993@gmail.com>
2022-01-29 16:24:10 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+import spacy
+
+def ner(string, lang):
+
+    model = f'{lang}_core_web_sm'
+
+    nlp = spacy.load(model)
+    parsed = nlp(string)
+
+    out_list = []
+    for token in parsed:
+
+        text = token.text
+        #Not sure what you mean by type, so including a few possiblities
+        word_type =  token.ent_type_, token.dep_, token.head.pos_
+        start_pos = token.idx
+        end_pos = start_pos + len(text)
+
+        out_dict = {'text':text,
+                    'type':word_type,
+                    'start_pos':start_pos,
+                    'end_pos':end_pos}
+        
+        out_list.append(out_dict)
+
+    print(out_list)
+    return(out_list)
+
+ner('I have $5 today','en')
